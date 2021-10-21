8d416e298e867a003560a764e9f6a7644b71115d
Jordan Skomal <jskomal@gmail.com>
2021-10-21 13:04:37 -0600

    Add Vehicle model to modelB.js

---
+++ models/modelB.js
@@ -0,0 +1,11 @@
+var vehicle = {
+  carMake: 'Honda',
+  carModel: 'CR-V',
+  carMileage: 86000,
+  carYear: 2008,
+  isCurrentlyRunning: false,
+  isFancy: false,
+  carDoors: ['driver-side door', 'passenger-side door', 'back driver-side door',
+'back passenger-side door', 'back hatch'],
+  carBestTimesToDrive: ['Late at night', 'Early in the morning']
+};
