6ffc76055439dacbc4584ff45f91c19a77c03909
Jordan Skomal <jskomal@gmail.com>
2021-10-21 12:58:35 -0600

    Add Airport model to modelA.js

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var airport = {
+  airportName: 'Denver International Airport',
+  airportAddress: '8500 Peña Blvd, Denver, CO 80249',
+  airportElevationInFeet: 5430,
+  airportNumOfRunways: 6,
+  isActive: true,
+  isClosed: false,
+  airportRestaurants: ['Starbucks', 'Root Down', 'McDonalds'],
+  airportAirlines: ['Southwest', 'Delta', 'United']
+};
